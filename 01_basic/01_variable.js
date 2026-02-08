ccc25a6aab65ed2db42e2d32b56b08b31fd38518
Ankit <ankitr.vaghela.171@gmail.com>
2026-02-08 07:05:40 +0000

    configure commit done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const accountId="1234"
+let accountEmail="ankit@gmail.com"
+var accountcity="jaipur"
+let accountstate;
+
+console.table([accountEmail,accountId,accountcity,accountstate])
+
+// not use var because block scope and functional scope//
